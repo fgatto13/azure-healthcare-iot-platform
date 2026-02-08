f2f57d7144af0280c5533543fc0a1887c32b4e90
fgatto13 <francescogatto2001@gmail.com>
2026-02-08 17:20:16 +0100

    started integrating Microsoft Entra ID login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,68 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuth } from "./context/AuthContext";
-import { Dashboard} from "./pages/Dashboard";
-import { Login } from "./pages/Login";
-import { Patient} from "./pages/Patient";
-import { Header } from "./components/layout/Header";
-import { Footer } from "./components/layout/Footer";
-import "./styles/App.css";
+import { MsalProvider, AuthenticatedTemplate, useMsal, UnauthenticatedTemplate } from '@azure/msal-react';
+import { Container, Button } from 'react-bootstrap';
+import { PageLayout } from './components/PageLayout';
+import { IdTokenData } from './components/DataDisplay';
+import { loginRequest } from './utils/authConfig';
 
-export const App = () => {
-  const { isAuthenticated } = useAuth();
+import './styles/App.css';
 
-  return (
-    <>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/login" element={<Login />} />
+/**
+* Most applications will need to conditionally render certain components based on whether a user is signed in or not. 
+* msal-react provides 2 easy ways to do this. AuthenticatedTemplate and UnauthenticatedTemplate components will 
+* only render their children if a user is authenticated or unauthenticated, respectively. For more, visit:
+* https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+*/
+const MainContent = () => {
+    /**
+    * useMsal is hook that returns the PublicClientApplication instance,
+    * that tells you what msal is currently doing. For more, visit:
+    * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/hooks.md
+    */
+    const { instance } = useMsal();
+    const activeAccount = instance.getActiveAccount();
 
-          {isAuthenticated ? (
-            <>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/patient/:id" element={<Patient />} />
-            </>
-          ) : (
-            <Route path="*" element={<Navigate to="/login" replace />} />
-          )}
-        </Routes>
-      </main>
-      <Footer/>
-    </>
-  );
-}
+    const handleRedirect = () => {
+        instance
+            .loginRedirect({
+                ...loginRequest,
+                prompt: 'create',
+            })
+            .catch((error) => console.log(error));
+    };
+    return (
+        <div className="App">
+            <AuthenticatedTemplate>
+                {activeAccount ? (
+                    <Container>
+                        <IdTokenData idTokenClaims={activeAccount.idTokenClaims} />
+                    </Container>
+                ) : null}
+            </AuthenticatedTemplate>
+            <UnauthenticatedTemplate>
+                <Button className="signInButton" onClick={handleRedirect} variant="primary">
+                    Sign up
+                </Button>
+            </UnauthenticatedTemplate>
+        </div>
+    );
+};
+
+
+/**
+* msal-react is built on the React context API and all parts of your app that require authentication must be 
+* wrapped in the MsalProvider component. You will first need to initialize an instance of PublicClientApplication 
+* then pass this to MsalProvider as a prop. All components underneath MsalProvider will have access to the 
+* PublicClientApplication instance via context as well as all hooks and components provided by msal-react. For more, visit:
+* https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+*/
+const App = ({ instance }) => {
+    return (
+        <MsalProvider instance={instance}>
+            <PageLayout>
+                <MainContent />
+            </PageLayout>
+        </MsalProvider>
+    );
+};
+
+export default App;
